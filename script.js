e8aed66897ec4eb7c623cb77607702175c8e6b02
Andre Rocha <99468080+andrerochadsr@users.noreply.github.com>
2022-11-20 21:14:29 -0300

    .

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function opera(num) {
     /*const plus = [];
     const multi = [];
@@ -14,21 +15,34 @@ function opera(num) {
         minus.push(document.getElementById('i/o').value)
         console.log(minus)
     } */
+
+    
     const numero = (num) => {
         return parseInt(num);
     };
-    console.log(numero(num))
+    //console.log(numero(num))
     
     const nume = num;
     //console.log(nume)
-    //alert(nume);
     document.getElementById('i/o').value += nume;
 }
 function result() {
     const resultado = document.getElementById('i/o').value;
+    //console.log(resultado)
     if (resultado.includes('+')) {
+        
         const re = resultado.split('+')
-        console.log(re)
+        //console.log(re)
+        re.forEach((element) => {
+            const numero = parseInt(element);
+            const soma = numero+numero;
+            console.log(soma);
+            return soma;
+        });
+        console.log(re);
     }
-    console.log(typeof(resultado));
+    
+    
+
+
 }
